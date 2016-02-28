347510a9e06b5d099f223f37f3d8522ae7c46b38
Lioncash <mathew1800@gmail.com>
2016-02-28 04:20:43 -0500

    Get rid of unnecessary batch files

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set CTRULIB=%DEVKITPRO%/libctru
-set HOME=D:/3ds/ninjhax
-set AEMSTRO=D:/3ds/aemstro
-set PATH=%PATH%;%HOME%
-echo %CTRULIB%
-make
-pause
--- clean.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set CTRULIB=%DEVKITPRO%/libctru-master
-set HOME=D:/3ds/ninjhax
-set PATH=%PATH%;%HOME%
-echo %CTRULIB%
-make clean
-pause
